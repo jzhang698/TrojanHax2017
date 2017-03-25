06ee5dade7af0aee26fa3df089ff95dc1d9d7c23
pelillian <plillian@usc.edu>
2017-03-25 12:38:36 -0700

    starting

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Trojan Hacks Spring 2017: Machine Learning
 
+# TrojanHealthBot
+
 For Spring 2017 Trojan Hacks, our team is implementing a machine learning algorithm to diagnose abnormal heart complications. This algorithm can be used to help save lives and make better predictions than doctors.
---
+++ src/TrojanHealthBot.py
@@ -0,0 +1,15 @@
+class TrojanHealthBot:
+    weights = []
+    bias = 0
+        
+    def train(filename):
+        trainFile = open(filename)
+        print(trainFile.read())
+
+    def test(filename):
+        testFile = open(filename)
+    
+    def __init__(self, numFeatures, trn, tst):
+        weights = [0] * numFeatures
+        train(trn)
+        test(tst)
